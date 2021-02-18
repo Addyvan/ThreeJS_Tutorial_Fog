e8d67dc59e625ea8ad5fb89ba1d8807033fc2bd4
addyvan <avandenhoeve14@ubishops.ca>
2021-02-18 18:48:11 -0500

    adding basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/main.js',
+  ],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  }
+};
